2275b1933714bdc29857556209a03bb8dc6d5de3
Clancy Mendonca <clancymendonca@gmail.com>
2024-01-30 22:57:59 +0530

    Update Shapes.jsx

--- clancymendonca/src/slices/Hero/Shapes.jsx
+++ clancymendonca/src/slices/Hero/Shapes.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber"; 
@@ -6,6 +6,12 @@ import { ContactShadows, Float, Environment } from "@react-three/drei";
 import { Suspense, useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
-export function name(params) {
-    
+export function Shapes() {
+    return (
+        <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+            <Canvas className="z-0" shadows>
+
+            </Canvas>
+        </div>
+    )
 }
