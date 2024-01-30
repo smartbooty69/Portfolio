b2523687e67420707c3250770c68743a34d70898
Clancy Mendonca <clancymendonca@gmail.com>
2024-01-30 22:34:38 +0530

    Portfolio Commit 1.1

---
+++ clancymendonca/src/slices/Hero/Shapes.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber"; 
+import { ContactShadows, Float, Environment } from "@react-three/drei";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+export function name(params) {
+    
+}
