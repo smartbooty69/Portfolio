1141ae7d61061a3660e7fad2d8d84504358e678b
Clancy Mendonca <clancymendonca@gmail.com>
2024-02-09 15:38:27 +0530

    About section

---
+++ clancymendonca/src/slices/Biography/Avatar.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { ImageField } from "@prismicio/client"
+import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
+import { useRef } from "react";
+
+
+type AvatarProps = {
+    image: ImageField;
+    className?: string;
+};
+
+export default function Avatar({ image, className}: AvatarProps){
+    const component = useRef(null);
+    return(
+        <div ref={component} className={clsx("relative h-full w-full", className)}>
+            <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
+                <PrismicNextImage
+                    field={image}
+                    className="avatar-image h-full object-fill"
+                    imgixParams={{q:90}}
+                />
+   
+            </div>
+        </div>
+    );    
+}
